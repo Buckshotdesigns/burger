acb55843617c4ee60e54ff8c6e2cf28220812647
bryanbuckus <bryanbuckus@gmail.com>
2019-04-30 16:32:28 -0400

    working on the submit burger

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,3 +1,17 @@
-$(".devour").on("click", function(event){
+$("#submit-burger").on("submit", function(event){
+    console.log("clicked")
     event.preventDefault();
-})
+    var newBurger = {
+        burger_name: burger,
+        devoured: false
+    };
+    $.ajax("/api/burger", {
+        type: "POST",
+        data: newBurger
+    }).then(
+        function() {
+            console.log("burger added");
+            location.reload();
+        }
+    );
+});
